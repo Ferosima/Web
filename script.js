28fe5fece4ab308e101f028deec071452397dfb4
Гусаченко Сергій <gusachenko02@gmail.com>
2020-08-02 02:21:37 +0300

    update

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ doc.backgroundImage = "url('image/menu/Place_Your_Image_Here_2.png')";
 doc.width = "316px";
 doc.height = " 316px";
 document.getElementById("three").style.backgroundImage = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\)_3.png')";
+
+let backgroundFunction = (doc_name, height = "613px", width = "611px", url = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\)_3.png')") => {
+    let doc = document.getElementById(doc_name).style
+    doc.backgroundImage = url;
+    doc.width = width;
+    doc.height = height;
+    doc.backgroundSize = "cover";
+}
+backgroundFunction("first_meal");
+backgroundFunction("second_meal", "667px", "667px", "url('image/menu/Place_Your_Image_Here_2.png')");
