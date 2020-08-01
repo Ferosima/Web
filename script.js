bf29b8ef537bf459009e13395e437f91503e98b9
Гусаченко Сергій <gusachenko02@gmail.com>
2020-08-01 22:34:04 +0300

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict"
+// document.getElementById('div[id="one"]').style.backgroundImage = "url('image/menu/Place_Your_Image_Here_(Double_Click_to_Edit)_2.png')"
+document.getElementById("one").style.backgroundImage = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\).png')";
+let doc = document.getElementById("two").style
+doc.backgroundImage = "url('image/menu/Place_Your_Image_Here_2.png')";
+doc.width = "316px";
+doc.height = " 316px";
+document.getElementById("three").style.backgroundImage = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\)_3.png')";
