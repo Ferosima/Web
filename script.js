c3eab2bdaf7cbd1aad6bd0a1447659718b80cfba
Гусаченко Сергій <gusachenko02@gmail.com>
2020-08-02 03:18:26 +0300

    update

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ doc.width = "316px";
 doc.height = " 316px";
 document.getElementById("three").style.backgroundImage = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\)_3.png')";
 
-let backgroundFunction = (doc_name, height = "613px", width = "611px", url = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\)_3.png')") => {
+let backgroundFunction = (doc_name, width = "611px", height = "613px",
+    url = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\)_3.png')",
+    margin_top = "", margin_left = "") => {
     let doc = document.getElementById(doc_name).style
     doc.backgroundImage = url;
     doc.width = width;
     doc.height = height;
     doc.backgroundSize = "cover";
+    if (margin_left != "")
+        doc.marginLeft = margin_left;
+    if (margin_top != "")
+        doc.marginTop = margin_top;
 }
 backgroundFunction("first_meal");
 backgroundFunction("second_meal", "667px", "667px", "url('image/menu/Place_Your_Image_Here_2.png')");
+// backgroundFunction("image_3", "824px", " 829px", "url('image/menu/pizza_1.png')");
+// backgroundFunction("image_3", "448px", "391px", "url('image/menu/pizza_2.png')", "874px", "306px")
