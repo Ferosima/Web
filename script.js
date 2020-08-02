acda51f05615b2af1d2ed99c1d077140808682ed
Гусаченко Сергій <gusachenko02@gmail.com>
2020-08-02 18:16:06 +0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 "use strict"
 // document.getElementById('div[id="one"]').style.backgroundImage = "url('image/menu/Place_Your_Image_Here_(Double_Click_to_Edit)_2.png')"
-document.getElementById("one").style.backgroundImage = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\).png')";
-let doc = document.getElementById("two").style
-doc.backgroundImage = "url('image/menu/Place_Your_Image_Here_2.png')";
-doc.width = "316px";
-doc.height = " 316px";
-document.getElementById("three").style.backgroundImage = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\)_3.png')";
+
 
 let backgroundFunction = (doc_name, width = "611px", height = "613px",
     url = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\)_3.png')",
@@ -20,7 +15,18 @@ let backgroundFunction = (doc_name, width = "611px", height = "613px",
     if (margin_top != "")
         doc.marginTop = margin_top;
 }
+
+document.getElementById("one").style.backgroundImage = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\).png')";
+let doc = document.getElementById("two").style
+doc.backgroundImage = "url('image/menu/Place_Your_Image_Here_2.png')";
+doc.width = "316px";
+doc.height = " 316px";
+document.getElementById("three").style.backgroundImage = "url('image/menu/Place_Your_Image_Here_\(Double_Click_to_Edit\)_3.png')";
+
 backgroundFunction("first_meal");
 backgroundFunction("second_meal", "667px", "667px", "url('image/menu/Place_Your_Image_Here_2.png')");
-// backgroundFunction("image_3", "824px", " 829px", "url('image/menu/pizza_1.png')");
-// backgroundFunction("image_3", "448px", "391px", "url('image/menu/pizza_2.png')", "874px", "306px")
+// let buttons = table.getElementsByTagName('button');
+let buttons = document.getElementsByTagName('button');
+for (let button of buttons) {
+    button.onclick = () => { alert("Click!") }
+}
